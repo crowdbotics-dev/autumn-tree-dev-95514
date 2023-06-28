cfeda1ac70040669928771f5e4c12a225734b166
Crowdbotics <engineering@crowdbotics.com>
2023-06-28 12:06:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-tree-dev-95514",
+  url: "https://autumn-tree-dev-95514.botics.co",
+  api: "https://autumn-tree-dev-95514.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
